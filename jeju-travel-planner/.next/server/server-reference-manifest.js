e83b11e07445841bf83de353e13dfbaa3f9d74e4
MGX <admin@mgx.dev>
2025-09-15 03:43:42 -0700

    Fixed build error.

--- jeju-travel-planner/.next/server/server-reference-manifest.js
+++ jeju-travel-planner/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2lNylbtqsPYCWIoA7TpFjGCjoRrSTRxhLOoJo73OEEw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IgcXjPVo4CAYLloAS1xODLzLQAoaItmherIReXK/VyM=\"}"
